5b9439512850d8831217b153448ab57f561d78c1
CitlaliHP <122800835+CitlaliHP@users.noreply.github.com>
2023-05-17 15:42:50 +0000

    update

--- main.sql
+++ main.sql
@@ -1,1 +1,11 @@
-SELECT "Hello world!";
+base.db;
+.open base.db;
+CREATE TABLE PERSONAS(
+  id_persona 	INTEGER	 PRIMARY KEY	 AUTOINCREMENT,
+  nombre 	text(30) 	NOT NULL,
+  primer_apellido 	text(30) NOT NULL,
+  segundo_apellido text(30) NOT NULL,
+  email text(30) NOT NULL );
+.mode csv
+.import registro.csv PERSONAS 
+select * from PERSONAS;
